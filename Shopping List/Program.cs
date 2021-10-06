6c26f1c5d688ecfe6d50680f20cf4cee7d36a4a6
delmar <delmarpre@gmail.com>
2021-10-06 08:57:13 -0400

    finished

---
+++ Shopping List/Program.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+
+namespace Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello hello wellcome to johns gas station");
+            int num = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Item\t         \tprice");
+            Console.WriteLine("=======\t=======\t=======");
+
+        }
+    }
+}
